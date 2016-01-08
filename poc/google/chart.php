354fca85b575294cbb57da88d70a2908504c0a7e
Ulrich Pech <ulrichpech@gmail.com>
2016-01-08 23:50:13 +0100

    remove old style

--- poc/google/chart.php
+++ poc/google/chart.php
@@ -55,35 +55,10 @@
                 }
             };
 
-            var classicOptions = {
-                title: 'Average Temperatures and Daylight in Iceland Throughout the Year',
-                width: 900,
-                height: 500,
-                // Gives each series an axis that matches the vAxes number below.
-                series: {
-                    0: {targetAxisIndex: 0},
-                    1: {targetAxisIndex: 1}
-                },
-                vAxes: {
-                    // Adds titles to each axis.
-                    0: {title: 'Temps (Celsius)'},
-                    1: {title: 'Daylight'}
-                },
-                hAxis: {
-                    ticks: [new Date(2014, 0), new Date(2014, 1), new Date(2014, 2), new Date(2014, 3),
-                        new Date(2014, 4),  new Date(2014, 5), new Date(2014, 6), new Date(2014, 7),
-                        new Date(2014, 8), new Date(2014, 9), new Date(2014, 10), new Date(2014, 11)
-                    ]
-                },
-                vAxis: {
-                    viewWindow: {
-                        max: 30
-                    }
-                }
-            };
-
             function drawMaterialChart() {
                 var materialChart = new google.charts.Line(chartDiv);
+                //alert(google.visualization.dataTableToCsv(data));
+
                 materialChart.draw(data, materialOptions);
             }
 
