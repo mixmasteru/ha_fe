07183048ff80342fd3b68446144bac845aecf80a
Ulrich Pech <ulrichpech@gmail.com>
2016-01-10 13:24:46 +0100

    change to classic chart

--- poc/google/chart.php
+++ poc/google/chart.php
@@ -16,49 +16,47 @@
             var chartDiv = document.getElementById('chart_div');
 
             var data = new google.visualization.DataTable();
-            data.addColumn('date', 'Month');
-            data.addColumn('number', "Average Temperature");
-            data.addColumn('number', "Average Hours of Daylight");
-
-            data.addRows([
-                [new Date(2014, 0),  -.5,  5.7],
-                [new Date(2014, 1),   .4,  8.7],
-                [new Date(2014, 2),   .5,   12],
-                [new Date(2014, 3),  2.9, 15.3],
-                [new Date(2014, 4),  6.3, 18.6],
-                [new Date(2014, 5),    9, 20.9],
-                [new Date(2014, 6), 10.6, 19.8],
-                [new Date(2014, 7), 10.3, 16.6],
-                [new Date(2014, 8),  7.4, 13.3],
-                [new Date(2014, 9),  4.4,  9.9],
-                [new Date(2014, 10), 1.1,  6.6],
-                [new Date(2014, 11), -.2,  4.5]
-            ]);
-
-            //alert(data.toJSON());
 
             var materialOptions = {
                 chart: {
-                    title: 'Average Temperatures and Daylight in Iceland Throughout the Year'
+                    title: 'Room Temperature and Humidity'
                 },
                 width: 900,
                 height: 500,
                 series: {
                     // Gives each series an axis name that matches the Y-axis below.
                     0: {axis: 'Temps'},
-                    1: {axis: 'Daylight'}
+                    1: {axis: 'Humidity'}
                 },
                 axes: {
                     // Adds labels to each axis; they don't have to match the axis names.
                     y: {
-                        Temps: {label: 'Temps (Celsius)'},
-                        Daylight: {label: 'Daylight'}
+                        Temps: {label: 'Temps (Celsius)',maxValue: 40},
+                        Humidity: {label: 'Humidity (%)'}
                     }
                 }
             };
 
+            var classicOptions = {
+                title: 'Room Temperature and Humidity',
+                width: 900,
+                height: 500,
+                // Gives each series an axis that matches the vAxes number below.
+                series: {
+                    0: {targetAxisIndex: 0},
+                    1: {targetAxisIndex: 1}
+                },
+                vAxes: {
+                    // Adds titles to each axis.
+                    0: {title: 'Temps (Celsius)', viewWindow: {min: 10, max: 40 }},
+                    1: {title: 'Humidity (%)', viewWindow: { min: 0, max: 100 }}
+                }
+            };
+
             function drawMaterialChart() {
-                var materialChart = new google.charts.Line(chartDiv);
+                // material:
+                // var chart = new google.charts.Line(chartDiv);
+                var chart = new google.visualization.LineChart(chartDiv);
                 //alert(google.visualization.dataTableToCsv(data));
 
                 var jsonData = $.ajax({
@@ -71,7 +69,7 @@
                 // Create our data table out of JSON data loaded from server.
                 var data1 = new google.visualization.DataTable(jsonData);
 
-                materialChart.draw(data1, materialOptions);
+                chart.draw(data1, classicOptions);
             }
 
             drawMaterialChart();
