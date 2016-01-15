cc6cd80feb383b43465a85e5519ef7c0da6ef9b9
Ulrich Pech <ulrichpech@gmail.com>
2016-01-15 21:38:29 +0100

    fix host

--- poc/d3/getData.php
+++ poc/d3/getData.php
@@ -3,7 +3,7 @@ $limit = 1;
 $auth = "";
 $host  = "";
 $json_temps = file_get_contents("http://".$auth.$host."/t/temps/1/0/".$limit."/");
-//$json_humis = file_get_contents("http://".$auth."api.bln2.de/t/humis/1/0/".$limit."/");
+//$json_humis = file_get_contents("http://".$auth.$host."/t/humis/1/0/".$limit."/");
 
 $arr_temps = json_decode($json_temps,true);
 
